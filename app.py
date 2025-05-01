5a1ca959dce04682807ca106722830ce04f2dd58
Akmal Nazir <akmal.nazirbusiness@gmail.com>
2025-05-01 19:07:35 -0400

    Auto-delete best_frame.jpg after display

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
-import os
 from utils.video_analyzer import analyze_video
 from utils.caption_generator import suggest_caption
+from threading import Thread
+
 import time
+import os
 
 load_dotenv()
 
@@ -16,6 +18,13 @@ def safe_remove(filepath, retries=5, delay=0.5):
             break
         except PermissionError:
             time.sleep(delay)
+
+def delete_file_later(filepath, delay=5):
+    time.sleep(delay)
+    try:
+        os.remove(filepath)
+    except Exception as e:
+        print(f"Failed to delete {filepath}: {e}")
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -32,6 +41,9 @@ def index():
             if os.path.exists(video_path):
                 safe_remove(video_path)
 
+        frame_path = os.path.join("static", analysis["best_frame_filename"])
+        Thread(target=delete_file_later, args=(frame_path,)).start()
+        
         return render_template('result.html', analysis=analysis, caption=new_caption)
 
     return render_template('index.html')
